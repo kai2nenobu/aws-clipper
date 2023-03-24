d3232bdd1ab3f4fbdbe5ea332ec40722fe8face0
Tsunenobu Kai <kai2nenobu@gmail.com>
2023-03-25 03:34:49 +0900

    chore: fix incompliant with linters

--- awscli_config/__init__.py
+++ awscli_config/__init__.py
@@ -1,19 +1,18 @@
 from __future__ import annotations
 
-from typing import Any, TextIO
-
 import yaml
+from typing import Any, TextIO
 
 
 def _subst_variables(dic: dict[str, Any], variables: dict[str, Any]) -> dict[str, Any]:
     return {k: v.format(**variables) if isinstance(v, str) else v for k, v in dic.items()}
 
 
-def convert(instream: TextIO, outstream: TextIO) -> None:
+def convert(instream: TextIO, outstream: TextIO):
     config = yaml.safe_load(instream)
     if config is None:
         return  # nothing to do
-    default_settings = config.get("default", {})
+    default_settings = config.get('default', {})
     profiles = {}
     for name, prof in config.get("profiles", {}).items():
         prof = {} if prof is None else prof
@@ -28,6 +27,7 @@ def convert(instream: TextIO, outstream: TextIO) -> None:
     # print(list(profiles))
     for name, prof in profiles.items():
         prof_name = "[default]" if name == "default" else f"[profile {name}]"
+        not_used = "varaible"
         outstream.write(prof_name + "\n")
         for k, v in prof.items():
             outstream.write(f"{k} = {v}\n")

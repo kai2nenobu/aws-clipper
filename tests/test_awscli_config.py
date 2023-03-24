d15b20bf55961cfd0fbb71acd78857da6b3095ae
Tsunenobu Kai <kai2nenobu@gmail.com>
2023-03-24 17:06:42 +0900

    Local input yaml and expected config in "data" dir

--- tests/test_awscli_config.py
+++ tests/test_awscli_config.py
@@ -1,41 +1,27 @@
-
 from io import StringIO
+from pathlib import Path
+
+import pytest
 
 from awscli_config import _convert
 
 
-def test_empty():
-    input_yaml = StringIO("")
+def all_test_data():
+    """Read from input yaml and expected config from "data" directory."""
+    data_dir = Path(__file__).parent / "data"
+    yamls = data_dir.glob("**/*.yaml")
+    def param(yaml_file: Path):
+        test_name = yaml_file.name
+        ini = yaml_file.with_suffix(".ini")
+        enc = "utf-8"
+        return pytest.param(yaml_file.read_text(encoding=enc), ini.read_text(encoding=enc), id=test_name)
+
+    return [param(yaml_file) for yaml_file in yamls]
+
+
+@pytest.mark.parametrize("input_yaml, expected_config", all_test_data())
+def test_data(input_yaml: str, expected_config: str):
+    input = StringIO(input_yaml)
     output = StringIO()
-    _convert(input_yaml, output)
-    assert output.getvalue() == ""
-
-
-def test_only_default_profile():
-    input_yaml = StringIO("""\
-profiles:
-  default:
-    region: us-east-1
-""")
-    output = StringIO()
-    _convert(input_yaml, output)
-    assert output.getvalue() == """\
-[default]
-region = us-east-1
-
-"""
-
-
-def test_single_named_profile():
-    input_yaml = StringIO("""\
-profiles:
-  my:
-    region: us-east-1
-""")
-    output = StringIO()
-    _convert(input_yaml, output)
-    assert output.getvalue() == """\
-[profile my]
-region = us-east-1
-
-"""
+    _convert(input, output)
+    assert output.getvalue() == expected_config

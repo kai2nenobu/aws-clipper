39f153057d5474bc5adbb9aa788d97dd33d55827
Tsunenobu Kai <kai2nenobu@gmail.com>
2023-03-27 00:32:09 +0900

    feat: read a input YAML file from CLI argument

--- aws_clipper/cli.py
+++ aws_clipper/cli.py
@@ -1,9 +1,24 @@
+from __future__ import annotations
+
+
 def main() -> None:
+    import argparse
     import sys
 
     from . import convert
 
-    convert(sys.stdin, sys.stdout)
+    parser = argparse.ArgumentParser(prog="aws-clipper")
+    parser.add_argument(
+        "input",
+        metavar="FILE",
+        nargs="?",
+        type=argparse.FileType("r", encoding="utf-8"),
+        default=sys.stdin,
+        help="Input YAML file",
+    )
+    args = parser.parse_args(sys.argv[1:])
+
+    convert(args.input, args.output)
 
 
 if __name__ == "__main__":
